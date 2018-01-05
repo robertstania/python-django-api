49da88d28cd127555a2fdf0b060483ce5ef13c80
Troy Amelotte <troy.amelotte@gTAef49b1.local>
2018-01-05 15:52:31 -0700

    done

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:id>', views.getOne),
     path('create', views.create),
+    path('delete/<int:id>', views.delete),
+    path('update/<int:id>', views.update)
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,15 +2,28 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import ast
 from .models import Question
 # Create your views here.
 def index(request):
     allQuestions = list(Question.objects.all().values())
     return JsonResponse({"questions": allQuestions}, safe=False)
 
+def getOne(request, id):
+    oneQuestion = list(Question.objects.filter(id=id).values())
+    return JsonResponse({"question": oneQuestion[0]}, safe=False)
+
+def delete(request, id):
+    Question.objects.get(id=id).delete()
+    return HttpResponse("deleted!")
+
 @csrf_exempt
 def create(request):
     createData = json.loads(request.body)
     Question.objects.create(question_text=createData["question_text"], pub_date=createData["pub_date"])
     return HttpResponse("All good")
+
+@csrf_exempt
+def update(request, id):
+    updateData = json.loads(request.body)
+    Question.objects.filter(id=id).update(question_text=updateData["question_text"], pub_date=updateData["pub_date"])
+    return HttpResponse("updated")
